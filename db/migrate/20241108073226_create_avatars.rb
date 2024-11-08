59e469a52192229d1f2c14a2700aa2c0acfda807
HijiriSato88 <satohiji0808@outlook.com>
2024-11-08 17:23:12 +0900

    テーブル定義

---
+++ db/migrate/20241108073226_create_avatars.rb
@@ -0,0 +1,13 @@
+class CreateAvatars < ActiveRecord::Migration[7.1]
+  def change
+    create_table :avatars, id:false do |t|
+      t.bigint :avatar_id, primary_key: true
+      t.integer :body, default: 0
+      t.integer :accessory, default: 0
+      t.boolean :logged_in, default: false
+      t.timestamps
+
+    end
+    add_foreign_key :users, :avatars, column: :avatar_id, primary_key: :avatar_id
+  end
+end
