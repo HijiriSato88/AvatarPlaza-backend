4ec3e1e586de82cf6c5f0d2c05698ceb1bc31215
Fuki <22rd080@ms.dendai.ac.jp>
2024-11-12 00:18:33 +0900

    Save changes to users_controller and add env file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+    #ここに呼び出されたらログインしている人を確認して、ログインしていたらその人のデータをjsonで取得するプログラムを書く
+    #今回はUser,Avaterテーブルがあるので、ログインしているAvaterの情報と共通しているカラムを利用してユーザー情報を取得する
+    #情報は二つのテーブル両方の全部のデータを取得する
+    def index
+        #@logged_in_users = User.joins(:avatar).where(avatars: { logged_in: true })
+        #render json: @logged_in_users
+    end
 end
