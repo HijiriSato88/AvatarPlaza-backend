7a702a40a36bb0f6f115928d123d2decefb0a99e
Fuki <22rd080@ms.dendai.ac.jp>
2024-11-15 12:03:26 +0900

    11/15/12/03

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     #今回はUser,Avaterテーブルがあるので、ログインしているAvaterの情報と共通しているカラムを利用してユーザー情報を取得する
     #情報は二つのテーブル両方の全部のデータを取得する
     def index
-        #@logged_in_users = User.joins(:avatar).where(avatars: { logged_in: true })
-        #render json: @logged_in_users
+        @logged_in_users=Users.joins(:avatars).where(avatars{logged_in:true})
+        render json:@logged_in_users
     end
 end
