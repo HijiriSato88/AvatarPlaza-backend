9578924fc91e39e25a2864ffb3c3dc4c9f1d2518
Fuki <22rd080@ms.dendai.ac.jp>
2024-11-15 15:52:12 +0900

    ログインユーザーの確認1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+    def index
+        #includesを使ってuser,avatarをくっつける。その中でlogged_inがtrueのやつをフィルタリングする。
+        @logged_in_users = User.includes(:avatar).where(avatars: { logged_in: true })
+        # includes を使って関連する Avatar データを一緒に取得。
+        render json: @logged_in_users,status: :ok
+    end
 end
