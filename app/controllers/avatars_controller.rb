953a4969fbd6f668bf8c2c2ac938561b6b06a3bc
shin22rd169 <matsumoto.452232@gmail.com>
2024-11-22 16:43:32 +0900

    ユーザ状態の更新を行うAPIの作成

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -2,11 +2,19 @@ class AvatarsController < ApplicationController
     before_action :set_avatar
 
     def update_accessory
-        
+       if @avatar.update(avatar_params)
+            render json: { message: "success to update accessory", avatar: @avatar  }, status: :ok
+       else
+            render json: { message: "failed to update accessory", details: @avatar.errors.full_message}, status: :unprocessable_entity
+       end
     end
 
     def update_comment
-        
+        if @avatar.update(comment: params[:comment])
+            render json: { message: "success to update comment", avatar: @avatar  }, status: :ok
+        else
+            render json: { message: "failed to update comment", details: @avatar.errors.full_message}, status: :unprocessable_entity
+        end
     end
 
     private
@@ -16,7 +24,6 @@ class AvatarsController < ApplicationController
     end
 
     def avatar_params
-        #commentカラム追加しだい、ここも変更
-        params.require(:avatar).permit(:accessory)
+        params.require(:avatar).permit(:accessory_body, :accessory_head, :accessory_leg)
     end
 end
