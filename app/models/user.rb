5dd7fe4a3feb3fefbb306d7f3e60d78d78fb6738
Fuki <22rd080@ms.dendai.ac.jp>
2024-11-22 11:17:46 +0900

    11/22/11:17バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
     validates:name,presence:true,format:{with:/\A[ぁ-んァ-ヶ一-龠々ーa-zA-Z]+\z/}
 
     #学籍番号のバリデーション,学籍番号の途中にRD,RMDの文字列がないといけない。それより前は2文字の数列、後は3文字の数列でないといけない。
-    validates:student_id,presence:true,format:{with/\A\d{2}(RD|RMD)\d{3}\z/}
+    validates:student_id,presence:true,format:{with/\A\d{2}(RD)\d{3}\z/}
     
     #メールアドレスのバリデーション,アドレス中の大文字小文字は区別している。
     validates:email,presence:true, uniqueness: { case_sensitive: true }
