f90130e0cd00e180a4f5d6c444438f1245a4c468
Fuki <22rd080@ms.dendai.ac.jp>
2024-11-22 11:07:09 +0900

    11/22/11:06バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
     belongs_to :avatar
     has_secure_password
+
+    #名前のバリデーション,ひらがな、かたかな、漢字、アルファベットのみ認める。
+    validates:name,presence:true,format:{with:/\A[ぁ-んァ-ヶ一-龠々ーa-zA-Z]+\z/}
+
+    #学籍番号のバリデーション,学籍番号の途中にRD,RMDの文字列がないといけない。それより前は2文字の数列、後は3文字の数列でないといけない。
+    validates:student_id,presence:true,format:{with/\A\d{2}(RD|RMD)\d{3}\z/}
+    
+    #メールアドレスのバリデーション,アドレス中の大文字小文字は区別している。
+    validates:email,presence:true, uniqueness: { case_sensitive: true }
+    
+    #パスワードのバリデーション,文字列の長さは6文字以上20文字以下でなければならない。
+    validates:password_digest,presence:true,length:{in:6..20}
 end
